6c3411173ea4fdf85a4b7e798f378d44c9e6e5a1
Jonathan Wilkinson <jonny041187@gmail.com>
2025-01-01 11:16:27 +0000

    Correct struct tag

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -12,7 +12,7 @@ type User struct {
 	ID        string    `json:"id"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
-	Email     string    `json:"string"`
+	Email     string    `json:"email_address"`
 	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 }
